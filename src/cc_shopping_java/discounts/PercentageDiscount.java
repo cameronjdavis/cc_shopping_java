e5f9fdc306e243c4be87256fc251fa7b0b0410f4
Cameron Davis <cameronjohndavis@gmail.com>
2016-09-17 11:40:04 +0100

    Percentage discount

---
+++ src/cc_shopping_java/discounts/PercentageDiscount.java
@@ -0,0 +1,31 @@
+package cc_shopping_java.discounts;
+
+/**
+ * A percentage discount.
+ * E.g. Discount total by 15%.
+ */
+public class PercentageDiscount implements Discount {
+
+	/**
+	 * Percentage amount, e.g. 0.15 = 15%.
+	 */
+	private double percentage;
+	
+	/**
+	 * @param percentage E.g. 0.15 = 15% discount.
+	 */
+	public PercentageDiscount(double percentage) {
+		this.percentage = percentage;
+	}
+	
+	/**
+	 * Apply a percentage discount to a total.
+	 */
+	@Override
+	public double applyDiscount(double total) {
+		double discountedTotal =total * (1.0 - this.percentage);
+		
+		return discountedTotal >= 0 ? discountedTotal : 0;
+	}
+
+}
---
+++ tests/cc_shopping_java/tests/discounts/PercentageDiscountTest.java
@@ -0,0 +1,80 @@
+package cc_shopping_java.tests.discounts;
+
+import static org.junit.Assert.*;
+import cc_shopping_java.discounts.*;
+import org.junit.Test;
+
+public class PercentageDiscountTest {
+
+	private PercentageDiscount subject;
+	
+	@Test
+	public void test_applyDiscount_noDiscount()
+    {
+		double total = 100;
+		double percentage = 0;
+		double expected = 100;
+		
+        this.subject = new PercentageDiscount(percentage);
+
+        double actual = this.subject.applyDiscount(total);
+
+        assertEquals(expected, actual, 0);
+    }
+	
+	@Test
+	public void test_applyDiscount_100PercentDiscount()
+    {
+		double total = 100;
+		double percentage = 1;
+		double expected = 0;
+		
+        this.subject = new PercentageDiscount(percentage);
+
+        double actual = this.subject.applyDiscount(total);
+
+        assertEquals(expected, actual, 0);
+    }
+	
+	@Test
+	public void test_applyDiscount_partialDiscount()
+    {
+		double total = 100;
+		double percentage = 0.15;
+		double expected = 85;
+		
+        this.subject = new PercentageDiscount(percentage);
+
+        double actual = this.subject.applyDiscount(total);
+
+        assertEquals(expected, actual, 0);
+    }
+	
+	@Test
+	public void test_applyDiscount_zeroTotal()
+    {
+		double total = 0;
+		double percentage = 0.15;
+		double expected = 0;
+		
+        this.subject = new PercentageDiscount(percentage);
+
+        double actual = this.subject.applyDiscount(total);
+
+        assertEquals(expected, actual, 0);
+    }
+	
+	@Test
+	public void test_applyDiscount_totalNeverNegative()
+    {
+		double total = 20;
+		double percentage = 1.15;
+		double expected = 0;
+		
+        this.subject = new PercentageDiscount(percentage);
+
+        double actual = this.subject.applyDiscount(total);
+
+        assertEquals(expected, actual, 0);
+    }
+}
