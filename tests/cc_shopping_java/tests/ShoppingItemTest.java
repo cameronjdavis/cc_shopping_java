556a4c448bc0f8e6b777c8d007ba4c21aa17a72a
Cameron Davis <cameronjohndavis@gmail.com>
2016-09-18 23:38:45 +0100

    Test for 100% coverage

---
+++ tests/cc_shopping_java/tests/ShoppingItemTest.java
@@ -0,0 +1,21 @@
+package cc_shopping_java.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cc_shopping_java.ShoppingItem;
+
+public class ShoppingItemTest {
+
+	private ShoppingItem subject;
+
+	@Test
+	public void test_onlyRunFor100PercentCoverage() {
+		this.subject = new ShoppingItem("PLU 1", "Name 1", 1.23);
+
+		assertEquals("PLU 1", this.subject.getPlu());
+		assertEquals("Name 1", this.subject.getName());
+		assertEquals(1.23, this.subject.getPrice(), 0);
+	}
+}
