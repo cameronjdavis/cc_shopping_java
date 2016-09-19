2d4e524ed1962a531ca518595e1e863a35461966
Cameron Davis <cameronjohndavis@gmail.com>
2016-09-19 09:14:07 +0100

    Renamed test

--- tests/cc_shopping_java/tests/ShoppingItemTest.java
+++ tests/cc_shopping_java/tests/ShoppingItemTest.java
@@ -11,7 +11,7 @@ public class ShoppingItemTest {
 	private ShoppingItem subject;
 
 	@Test
-	public void test_setClassVariables() {
+	public void test_setInstanceVariables() {
 		this.subject = new ShoppingItem("PLU 1", "Name 1", 1.23);
 
 		assertEquals("PLU 1", this.subject.getPlu());
